5d04fbd1147894006602d35ddc75ba5e9532257e
Brian Vehige <brianvehige@gmail.com>
2020-04-02 05:02:16 +0000

    first commit

---
+++ lib/tennis_racquets.rb
@@ -0,0 +1,6 @@
+require "tennis_racquets/version"
+
+module TennisRacquets
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tennis_racquets/version.rb
@@ -0,0 +1,3 @@
+module TennisRacquets
+  VERSION = "0.1.0"
+end
