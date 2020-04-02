78de59a2a4a52c0aaf2169bd4d7bb9b1e0fb8434
Brian Vehige <brianvehige@gmail.com>
2020-04-02 05:59:57 +0000

    fixing a few file errors

--- lib/tennis_racquets.rb
+++ lib/tennis_racquets.rb
@@ -1,4 +1,11 @@
-require "tennis_racquets/version"
+#require "tennis_racquets/version"
+require "pry"
+require "nokogiri"
+require "open-uri"
+
+require_relative "./tennis_racquets/cli.rb"
+require_relative "./tennis_racquets/scraper.rb"
+require_relative "./tennis_racquets/racquets.rb"
 
 module TennisRacquets
   class Error < StandardError; end
